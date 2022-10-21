9f065d701c0ca4b734777bcc056baa9da42ec13d
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-10-21 14:02:00 +0400

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,123 @@
-import React from 'react';
+import React from "react";
+import { footerData } from "../data";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const staggerContainer = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+    },
+  },
+};
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const { about, links, program, newsletter } = footerData;
+  return (
+    <footer className="bg-dark section">
+      <div className="container mx-auto">
+        {/* Grid */}
+        <motion.div
+          variants={staggerContainer}
+          initial={"hidden"}
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.1 }}
+          className="text-white flex flex-col lg:flex-row lg:justify-between gap-x-5 gap-y-14"
+        >
+          {/* About */}
+          <motion.div
+            variants={fadeIn("up")}
+            className="flex-1 flex flex-col gap-y-6"
+          >
+            {/* Title */}
+            <div className="font-primary text-xl uppercase tracking-[0.08em]">
+              {about.title}
+            </div>
+            {/* Subtitle */}
+            <div className="leading-relaxed text-[#dbdbdb]">
+              {about.subtitle}
+            </div>
+            {/* Address, Phone & Email */}
+            <div className="flex flex-col gap-y-4 font-semibold text-[#dbdbdb]">
+              {/* Address */}
+              <div className="flex items-center gap-x-[10px]">
+                <div>{about.address.icon}</div>
+                <div>{about.address.name}</div>
+              </div>
+              {/* Phone */}
+              <div className="flex items-center gap-x-[10px]">
+                <div>{about.phone.icon}</div>
+                <div>{about.phone.number}</div>
+              </div>
+              {/* Email */}
+              <div className="flex items-center gap-x-[10px]">
+                <div>{about.email.icon}</div>
+                <div>{about.email.address}</div>
+              </div>
+            </div>
+          </motion.div>
+          {/* Links */}
+          <motion.div
+            variants={fadeIn("up")}
+            className="flex-1 flex flex-col xl:items-center"
+          >
+            <div>
+              <div className="font-primary text-xl uppercase tracking-[0.08em] mb-6">
+                {links.title}
+              </div>
+              <ul className="flex flex-col gap-y-4 text-[#dbdbdb]">
+                {links.items.map((item, index) => {
+                  const { href, name } = item;
+                  return (
+                    <li key={index}>
+                      <a href={href} className="hover:text-white transition">
+                        {name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </motion.div>
+          {/* Program */}
+          <motion.div variants={fadeIn("up")} className="flex-1">
+            <div className="font-primary text-xl uppercase tracking-[0.08em] mb-6">
+              {program.title}
+            </div>
+            <ul className="flex flex-col gap-y-4 text-[#dbdbdb]">
+              {program.items.map((item, index) => {
+                return <li key={index}>{item.name}</li>;
+              })}
+            </ul>
+          </motion.div>
+          {/* Newsletter */}
+          <motion.div variants={fadeIn("up")} className="flex-1">
+            <div className="font-primary text-xl uppercase tracking-[0.08em] mb-6">
+              {newsletter.subtitle}
+            </div>
+            <div className="leading-relaxed mb-9 text-[#dbdbdb]">
+              {newsletter.subtitle}
+            </div>
+            <form className="flex justify-between items-start border-b border-[#b6b6b6]">
+              <input
+                className="outline-none placeholder:text-base italic placeholder:capitalize bg-transparent pb-2"
+                type="email"
+                placeholder={newsletter.form.placeholder}
+              />
+              <button
+                type="submit"
+                className="text-2xl cursor-pointer hover:scale-[1.1] transition ease-in duration-[250ms]"
+              >
+                {newsletter.form.icon}
+              </button>
+            </form>
+          </motion.div>
+        </motion.div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
